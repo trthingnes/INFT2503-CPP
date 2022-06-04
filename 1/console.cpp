451fb26a3ac5d155703ae9f2203f5ae4864ae817
Tobias <tobiasth@stud.ntnu.no>
2022-06-04 23:34:41 +0200

    Finished console program

---
+++ 1/console.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+const int length = 5;
+
+int main() {
+    cout << "Du må skrive " << length << " tall." << endl;
+
+    int low = 0, mid = 0, high = 0;
+    double temp;
+
+    for (int i = 1; i <= length; i++) {
+        cout << "Temperatur " << i << ": ";
+        cin >> temp;
+
+        if (temp < 10) {
+            low++;
+        }
+        else if (temp > 20) {
+            high++;
+        }
+        else {
+            mid++;
+        }
+    }
+
+    cout << "Du skrev inn " << low << " lav(e), " << mid << " middels og " << high << " høy(e) temperaturer." << endl;
+
+    return 0;
+}
